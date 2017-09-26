2e934f3a3c5a854a0df4866c2fade7754df502cf
nmb <nickbowers999@gmail.com>
2017-09-25 21:11:59 -0500

    Added in additional functional testing and incorporated index.html to be rendered.

--- lists/views.py
+++ lists/views.py
@@ -1,9 +1,9 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 
 def home_page(request):
-    return HttpResponse('<html><title>To-Do</title></html>')
+    context = {}
+    return render(request, 'index.html', context)
 
