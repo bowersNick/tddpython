6e0f2653ee3d442009aeff95746ba77476daf30d
nmb <nickbowers999@gmail.com>
2017-09-29 13:17:03 -0500

    Tests passing with ties to database.

--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Item
 
 # Create your views here.
@@ -6,11 +6,8 @@ from .models import Item
 
 def home_page(request):
     if request.method == 'POST':
-        new_item_text = request.POST['item_next']
-        Item.objects.create(text=new_item_text)
-    else:
-        new_item_text = ''
-
-    context = {'new_item_text': new_item_text}
+        Item.objects.create(text=request.POST['item_next'])
+        return redirect('/')
+    context = {'items': Item.objects.all()}
     return render(request, 'home.html', context)
 
