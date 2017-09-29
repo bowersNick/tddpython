cc8e73ca056202f720834875475d5022bdd2e909
nmb <nickbowers999@gmail.com>
2017-09-29 11:26:26 -0500

    Added in testing for items in database and refactored views.py to handle either a POST request or GET request.

--- lists/views.py
+++ lists/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
+from .models import Item
 
 # Create your views here.
 
 
 def home_page(request):
-    context = {'new_item_text': request.POST.get('item_next', '')}
+    if request.method == 'POST':
+        new_item_text = request.POST['item_next']
+        Item.objects.create(text=new_item_text)
+    else:
+        new_item_text = ''
+
+    context = {'new_item_text': new_item_text}
     return render(request, 'home.html', context)
 
